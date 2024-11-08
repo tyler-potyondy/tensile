1ef17823f2fb9850e81a55a01c662572907c0811
Tyler Potyondy <potyondyt@gmail.com>
2024-11-08 12:08:55 -0800

    Update setup script

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## Tensile
 
-Tensile is a Tock CI platform for testing thread and 15.4 network stacks.
+Tensile is a Tock CI platform for testing thread and 15.4 network stacks
+on nordic semiconductor devices.
 
-To setup the environment run the `./setup.sh` script.
+To setup the environment run: `source ./setup.sh`.
 
 To run the test suite, run `python3 main.py`.
--- setup.sh
+++ setup.sh
@@ -1,8 +1,42 @@
-# Create venv.
+# Script to create venv, activate, install dependencies. 
 
+
+# Install nordic tools (this includes Segger JLink and dependencies needed for pynrfjprog).
+# Download the .deb file from the nRF website if does not already exist.
+if [ ! -f nrf-command-line-tools_10.24.2_arm64.deb ]; then
+    wget https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x/10-24-2/nrf-command-line-tools_10.24.2_arm64.deb
+fi
+
+
+sudo apt install ./nrf-command-line-tools_10.24.2_arm64.deb -y 
+# Fix broken dependencies (JLink doesn't automatically install for some reason).
+sudo apt install /opt/nrf-command-line-tools/share/JLink_Linux_V794e_arm64.deb --fix-broken -y
+
+# Install other dependencies that are needed.
+sudo DEBIAN_FRONTEND=noninteractive apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install -y \
+           git gcc-arm-none-eabi make gcc-riscv64-unknown-elf \
+
+# Install / setup rust. (@Leon this should be redundant, but I was having issues
+# getting cargo working without this).
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Create venv / activate.
 python3 -m venv tensile-venv
 source tensile-venv/bin/activate
 
+# Install needed python dependencies.
 pip install -r requirements.txt
 
-git submodules update --init
+# NOTE: The testing scripts expect these repos to be in the same directory as main.py.
+# Clone libtock-c / tock / tockloader (need to clone tockloader from my repo temporarily 
+# until the tockloader jlink-selector branch is merged).
+
+git clone https://github.com/tock/tock.git
+git clone https://github.com/tock/libtock-c.git
+git clone https://github.com/tyler-potyondy/tockloader.git
+
+# TEMPORARY FIX until merged.
+cd tockloader && git checkout jlink-selector && cd ..
+
+
