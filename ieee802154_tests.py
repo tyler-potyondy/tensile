4979526926a637bdee47a230b78d63a7a5960ce0
Tyler Potyondy <potyondyt@gmail.com>
2024-11-08 10:33:31 -0800

    Add Test: ieee802154 radio_tx.

--- ieee802154_tests.py
+++ ieee802154_tests.py
@@ -16,4 +16,19 @@ def radio_rxtx_test(boards):
     board.prep_test()
 
     # Run test for 10 seconds and gather results.
-    test_results = board.run_test(10)
+    test_tx_results = board.run_test(10)
+
+    # The TX test transmits a packet every 250ms. For a 10s test, we expect 40 packets
+    # to be succesfully transmitted. We check that 95% of packets do not fail. This
+    # app prints "Transmitted succesfully" for each successful transmission.
+    success_passed = 0
+    TOTAL_PACKETS = 40
+    for result in test_tx_results:
+        if "Transmitted succesfully" in result:
+            success_passed += 1
+    
+    # Check if 95% of packets were transmitted successfully.
+    if success_passed / TOTAL_PACKETS >= 0.95:
+        board.log_info("PASSED: radio_tx test")
+    else:
+        raise Exception("FAILED: radio_tx test -- {} out of {} packets transmitted successfully.".format(success_passed, TOTAL_PACKETS))
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pynrfjprog import LowLevel 
-from board import Board
+from ieee802154_tests import radio_rxtx_test
 
 if __name__ == '__main__':
     # Scan for available devices.
     nrfjprog_api = LowLevel.API()
     nrfjprog_api.open()
-
     available_devices = nrfjprog_api.enum_emu_snr()
-
+    nrfjprog_api.close()
+    radio_rxtx_test(available_devices)
